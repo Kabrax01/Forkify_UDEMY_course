bf8c15bff6bacf3bb2a9b6dae4471be213674ecc
Kabrax01 <rogalski.arnold@gmail.com>
2023-11-12 11:00:28 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = 'f5a679b1-02d8-45db-aef4-d6c2eefce84d';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
